fdce63a7505201cd685435afcd8d8d9fa7c2cef2
Alfredo Monteiro <amonteiro.alfredo@gmail.com>
2024-03-22 08:59:57 -0100

    add expandable accounts array; refactor AccountDirectory api;add features deposit and withdraw

--- AccountDirectory.java
+++ AccountDirectory.java
@@ -3,10 +3,46 @@ import java.time.LocalDateTime;
 import ajp.SimpleIo;
 
 public class AccountDirectory {
+    private static final int DEFAULT_INITIAL_CAPACITY = 2;
+    private static int numberOfAccounts = 0;
+    private static Account[] accounts = new Account[DEFAULT_INITIAL_CAPACITY];
+
+    private AccountDirectory() {
+    }
+
+    private static void ensureCapacity() {
+        if (accounts.length == numberOfAccounts) {
+            Account[] copy = new Account[2 * accounts.length + 1];
+            System.arraycopy(accounts, 0, copy, 0, accounts.length);
+            accounts = copy;
+            copy = null;
+        }
+    }
+
+    private static void push(Account a) {
+        ensureCapacity();
+        accounts[numberOfAccounts] = a;
+        numberOfAccounts += 1;
+    }
+
+    private static Account pull(int accountNumber) {
+        int i = 0;
+        for (; i < numberOfAccounts; i++) {
+            if (accounts[i].id == accountNumber)
+                break;
+        }
+
+        Account result = accounts[i];
+        accounts[i] = accounts[numberOfAccounts - 1];
+        numberOfAccounts -= 1;
+
+        // Null the obsolete reference and avoids memory leak
+        accounts[numberOfAccounts] = null;
+
+        return result;
+    }
 
     public static void main(String[] args) {
-        Account[] accounts = new Account[10];
-        int numberOfAccounts = 0;
         int accountSelected = -1;
         String command;
 
@@ -25,17 +61,18 @@ public class AccountDirectory {
                                 "    Age: " + accounts[accountSelected].age + "\n" +
                                 "    Balance: " + accounts[accountSelected].balance + "\n");
 
-                SimpleIo.prompt("\nCommands: 3=deposit, 4=withdraw, 5=close, 6=back, 0=exit\n");
+                SimpleIo.prompt("\nCommands: 3=deposit, 4=withdraw, 5=close, 6=archive, 7=back, 0=exit\n");
                 SimpleIo.prompt("Enter command: ");
                 command = SimpleIo.readLine().trim();
             }
 
             switch (command) {
-                case "0":
+                case "0": {
                     System.out.println("Bye Bye!!!");
                     return;
-                case "1":
-                    System.out.println("*** Create Account ***");
+                }
+                case "1": {
+                    System.out.println("*** Create Account(" + accounts.length + ") ***");
                     SimpleIo.prompt("Enter your name: ");
                     String name = SimpleIo.readLine().trim();
                     SimpleIo.prompt("Enter your Social Security Number: ");
@@ -45,17 +82,14 @@ public class AccountDirectory {
                     SimpleIo.prompt("Enter the amount: ");
                     String inputAmount = SimpleIo.readLine().trim();
                     double amount = Double.parseDouble(inputAmount);
-                    accounts[numberOfAccounts] = new Account.Builder(
-                            name,
-                            ssn,
-                            age,
-                            amount,
-                            Account.Type.CHECKING)
-                            .email("al@gmail.com")
-                            .build();
-                    numberOfAccounts++;
+
+                    Account newAccount = new Account.Builder(numberOfAccounts, name, ssn, age, amount,
+                            Account.Type.CHECKING).build();
+                    AccountDirectory.push(newAccount);
+
                     break;
-                case "2":
+                }
+                case "2": {
                     System.out.println("*** Accounts ***");
                     for (int i = 0; i < numberOfAccounts; i++) {
                         System.out.println(i + " - " + accounts[i].name);
@@ -63,19 +97,35 @@ public class AccountDirectory {
                     SimpleIo.prompt("Select an account: ");
                     accountSelected = Integer.valueOf(SimpleIo.readLine().trim());
                     break;
-                case "3":
-                    System.out.println("*** Withdraw Money ***");
+                }
+                case "3": {
+                    System.out.println("*** Deposit Money ***");
+                    SimpleIo.prompt("Enter the amount: ");
+                    String value = SimpleIo.readLine().trim();
+                    double amount = Double.parseDouble(value);
+                    if (accountSelected != -1 && amount > 0) {
+                        accounts[accountSelected].deposit(amount);
+                    }
                     break;
-                case "4":
-                    System.out.println("*** Close Account ***");
+                }
+                case "4": {
+                    System.out.println("*** Withdraw Account ***");
+                    SimpleIo.prompt("Enter the amount: ");
+                    String value = SimpleIo.readLine().trim();
+                    double amount = Double.parseDouble(value);
+                    if (accountSelected != -1 && amount > 0) {
+                        accounts[accountSelected].withdraw(amount);
+                    }
                     break;
-                case "6":
+                }
+                case "7": {
                     accountSelected = -1;
                     break;
-
-                default:
+                }
+                default: {
                     SimpleIo.prompt("Commands: 1=create, 2=deposit, 3=withdraw, 4=close, 0=exit\n");
                     break;
+                }
             }
         }
 
@@ -95,6 +145,7 @@ class Account {
 
     // Instance Fields
     // Required fields
+    int id;
     String name;
     String ssn;
     int age;
@@ -108,6 +159,7 @@ class Account {
     Status status;
 
     private Account(Builder builder) {
+        id = builder.id;
         name = builder.name;
         ssn = builder.ssn;
         age = builder.age;
@@ -119,14 +171,15 @@ class Account {
         createdAt = builder.createdAt;
     }
 
-    public double deposit(double amount) {
+    public void deposit(double amount) {
         balance += amount;
-        return balance;
     }
 
-    public double withdraw(double amount) {
+    public void withdraw(double amount) {
+        if ((balance - amount) < 0)
+            return;
+
         balance -= amount;
-        return balance;
     }
 
     public void close() {
@@ -136,6 +189,7 @@ class Account {
 
     public static class Builder {
         // Required fields
+        private final int id;
         private final String name;
         private final String ssn;
         private final int age;
@@ -148,7 +202,8 @@ class Account {
         private Status status = Status.OPEN;
         private final LocalDateTime createdAt = LocalDateTime.now();
 
-        public Builder(String name, String ssn, int age, double initialBalance, Type type) {
+        public Builder(int id, String name, String ssn, int age, double initialBalance, Type type) {
+            this.id = id;
             this.name = name;
             this.ssn = ssn;
             this.age = age;
